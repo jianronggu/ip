90d345ebe51d41c4c2bd7a3058a8b58ff5939b21
jianronggu <e1398350@u.nus.edu>
2025-08-24 22:47:49 +0800

    Level-1

--- src/main/java/Labussy.java
+++ src/main/java/Labussy.java
@@ -1,10 +1,32 @@
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Labussy {
+
+
+    private static void divide() {
+        String Line = "____________________________________________________________";
+        System.out.println(Line);
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        divide();
+        System.out.println("Hello! I'm Labussy.");
+        System.out.println("What can I do for you?");
+        divide();
+
+        while (true) {
+            String word = scanner.nextLine();
+            if (word.equals("bye")) {
+                divide();
+                System.out.println("Bye. Hope to see you again soon!");
+                divide();
+                break;
+            }
+                divide();
+                System.out.println(word);
+                divide();
+            }
+        }
 }
+
