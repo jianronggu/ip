0c042fad606c02e902b940df052b4ae0fabbfab0
jianronggu <e1398350@u.nus.edu>
2025-08-25 02:35:36 +0800

    Level-2

--- src/main/java/Labussy.java
+++ src/main/java/Labussy.java
@@ -1,15 +1,18 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Labussy {
-
 
     private static void divide() {
         String Line = "____________________________________________________________";
         System.out.println(Line);
     }
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> storage = new ArrayList<>();
+
         divide();
         System.out.println("Hello! I'm Labussy.");
         System.out.println("What can I do for you?");
@@ -23,9 +26,18 @@ public class Labussy {
                 divide();
                 break;
             }
+            if (word.equals("list")) {
                 divide();
-                System.out.println(word);
+                 for (int i = 0; i < storage.size(); i++) {
+                    System.out.println(i + "." + storage.get(i));
+                    break;
+                }
                 divide();
+            }
+            storage.add(word);
+            divide();
+            System.out.println("added: " + word);
+            divide();
             }
         }
 }
