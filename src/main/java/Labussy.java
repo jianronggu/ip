fec143c7d36fb6e6e5fc45f579e85b2799b2de8e
jianronggu <e1398350@u.nus.edu>
2025-08-25 10:37:25 +0800

    Level-3

--- src/main/java/Labussy.java
+++ src/main/java/Labussy.java
@@ -8,10 +8,20 @@ public class Labussy {
         String Line = "____________________________________________________________";
         System.out.println(Line);
     }
+
+    private static void printList(ArrayList<Task> tasks) {
+        divide();
+        System.out.println("Here are the tasks in your list: ");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.get(i));
+        }
+        divide();
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> storage = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         divide();
         System.out.println("Hello! I'm Labussy.");
@@ -19,26 +29,51 @@ public class Labussy {
         divide();
 
         while (true) {
-            String word = scanner.nextLine();
-            if (word.equals("bye")) {
+            String input = scanner.nextLine();
+            Task task = new Task(input);
+
+            if (input.equals("bye")) {
                 divide();
                 System.out.println("Bye. Hope to see you again soon!");
                 divide();
                 break;
             }
-            if (word.equals("list")) {
-                divide();
-                 for (int i = 0; i < storage.size(); i++) {
-                    System.out.println(i + "." + storage.get(i));
-                    break;
-                }
-                divide();
+
+            if (input.equals("list")) {
+                printList(tasks);
+                continue;
             }
-            storage.add(word);
-            divide();
-            System.out.println("added: " + word);
-            divide();
+
+            if (input.toLowerCase().startsWith("mark ")) {
+                String[] parts = input.split("\\s+", 2);
+
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks.get(index).markAsDone();
+                divide();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(index));
+                divide();
+                continue;
+            }
+
+            if (input.toLowerCase().startsWith("unmark" )) {
+                String[] parts = input.split("\\s+", 2);
+
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks.get(index).markAsUndone();
+                divide();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(tasks.get(index));
+                divide();
+                continue;
+            }
+            else {
+                tasks.add(task);
+                divide();
+                System.out.println("added: " + input);
+                divide();
             }
         }
+    }
 }
 
