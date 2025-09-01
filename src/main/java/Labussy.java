c405323390c4791a3c6f07b59f66e7e6bac2d202
jianronggu <e1398350@u.nus.edu>
2025-09-02 00:29:09 +0800

    Clean up.

--- src/main/java/Labussy.java
+++ src/main/java/Labussy.java
@@ -1,21 +1,7 @@
-import javax.lang.model.type.NullType;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 
 public class Labussy {
-    private static void divide() {
-        String Line = "____________________________________________________________";
-        System.out.println(Line);
-    }
-
-    private static void printList(ArrayList<Task> tasks) {
-        divide();
-        System.out.println("Here are the tasks in your list: ");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println(" " + (i + 1) + "." + tasks.get(i));
-        }
-        divide();
-    }
 
     public static void main(String[] args) {
 
@@ -28,102 +14,96 @@ public class Labussy {
 
         while(true) {
             String input = ui.readCommand();
-            switch (Parser.kind(input)) {
-                case BYE:
-                    ui.showBye();
-                    System.exit(0);
+            try {
+                switch (Parser.kind(input)) {
+                    case BYE:
+                        ui.showBye();
+                        System.exit(0);
 
-                case LIST:
-                    ui.showList(tasks);
-                    break;
+                    case LIST:
+                        ui.showList(tasks);
+                        break;
 
-                case TODO: {
-                    try {
-                        String description = Parser.todoDesc(input);
-                        ToDo t = new ToDo(description);
-                        tasks.add(t);
+                    case TODO: {
+                        try {
+                            String description = Parser.todoDesc(input);
+                            ToDo t = new ToDo(description);
+                            tasks.add(t);
+                            storage.save(tasks.all());
+                            ui.showAdded(t, tasks.size());
+                            break;
+                        } catch (BlankException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+
+                    case DEADLINE: {
+                            String[] p = Parser.deadlineParts(input);
+                            Deadline d = new Deadline(p[0], new Dates(p[1]));
+                            tasks.add(d);
+                            storage.save(tasks.all());
+                            ui.showAdded(d, tasks.size());
+                            break;
+
+                    }
+
+                    case EVENT: {
+                            String[] p = Parser.eventParts(input);     // [desc, from, to]
+                            Event e = new Event(p[0], new Dates(p[1]), new Dates(p[2]));
+                            tasks.add(e);
+                            storage.save(tasks.all());
+                            ui.showAdded(e, tasks.size());
+                            break;
+                    }
+
+
+                    case MARK: {
+                        int idx = Parser.index1(input, "mark ") - 1;   // 0-based
+                        if (idx < 0 || idx >= tasks.size()) {
+                            ui.showError("Invalid task number");
+                            break;
+                        }
+                        tasks.get(idx).markAsDone();
                         storage.save(tasks.all());
-                        ui.showAdded(t, tasks.size());
+                        ui.showMarked(tasks.get(idx));
                         break;
-                    } catch (BlankException e) {
-                        System.out.println(e.getMessage());
                     }
-                }
 
-                case DEADLINE: {
-                    try {
-                        String[] p = Parser.deadlineParts(input);
-                        Deadline d = new Deadline(p[0], new Dates(p[1]));
-                        tasks.add(d);
+                    case UNMARK: {
+                        int idx = Parser.index1(input, "unmark ") - 1;
+                        if (idx < 0 || idx >= tasks.size()) {
+                            ui.showError("Invalid task number");
+                            break;
+                        }
+                        tasks.get(idx).markAsUndone();
                         storage.save(tasks.all());
-                        ui.showAdded(d, tasks.size());
+                        ui.showUnmarked(tasks.get(idx));
                         break;
-                    } catch (MissingComponentException e) {
-                        System.out.println(e.getMessage());
-                    } catch (BlankException e) {
-                        System.out.println(e.getMessage());
                     }
-                }
 
-                case EVENT: {
-                    try {
-                        String[] p = Parser.eventParts(input);     // [desc, from, to]
-                        Event e = new Event(p[0], new Dates(p[1]), new Dates(p[2]));
-                        tasks.add(e);
+                    case DELETE: {
+                        int idx = Parser.index1(input, "delete ") - 1;
+                        if (idx < 0 || idx >= tasks.size()) {
+                            ui.showError("Invalid task number");
+                            break;
+                        }
+                        Task removed = tasks.delete(idx);
                         storage.save(tasks.all());
-                        ui.showAdded(e, tasks.size());
-                        break;
-                    } catch (MissingComponentException e) {
-                        System.out.println(e.getMessage());
-                    } catch (BlankException e) {
-                        System.out.println(e.getMessage());
-                    }
-                }
-
-
-                case MARK: {
-                    int idx = Parser.index1(input, "mark ") - 1;   // 0-based
-                    if (idx < 0 || idx >= tasks.size()) {
-                        ui.showError("Invalid task number");
+                        ui.showRemoved(removed, tasks.size());
                         break;
                     }
-                    tasks.get(idx).markAsDone();
-                    storage.save(tasks.all());
-                    ui.showMarked(tasks.get(idx));
-                    break;
-                }
 
-                case UNMARK: {
-                    int idx = Parser.index1(input, "unmark ") - 1;
-                    if (idx < 0 || idx >= tasks.size()) {
-                        ui.showError("Invalid task number");
-                        break;
-                    }
-                    tasks.get(idx).markAsUndone();
-                    storage.save(tasks.all());
-                    ui.showUnmarked(tasks.get(idx));
-                    break;
+                    default:
+                        ui.showLine();
+                        ui.showError("I'm sorry, but I don't know what that means.");
+                        ui.showLine();
                 }
-
-                case DELETE: {
-                    int idx = Parser.index1(input, "delete ") - 1;
-                    if (idx < 0 || idx >= tasks.size()) {
-                        ui.showError("Invalid task number");
-                        break;
-                    }
-                    Task removed = tasks.delete(idx);
-                    storage.save(tasks.all());
-                    ui.showRemoved(removed, tasks.size());
-                    break;
-                }
-
-                default:
-                    ui.showLine();
-                    ui.showError("I'm sorry, but I don't know what that means.");
-                    ui.showLine();
+            } catch (MissingComponentException e) {
+            System.out.println(e.getMessage());
+            } catch (BlankException e) {
+                System.out.println(e.getMessage());
             }
         }
-
 
     }
 }
